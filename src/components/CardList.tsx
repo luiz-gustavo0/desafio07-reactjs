7b9031c5e8305e78c4d833297a7a158e0f72bbcf
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-02-09 22:30:48 -0300

    desafio finalizado

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,32 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { onClose, onOpen, isOpen } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [selectedImgUrl, setSelectedImgUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string): void => {
+    setSelectedImgUrl(url);
+    onOpen();
+  };
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImgUrl}
+      />
     </>
   );
 }
